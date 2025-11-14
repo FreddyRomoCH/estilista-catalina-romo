fd274f2b0ad4914d58b4282a1c7dd13fcd06b9ec
Freddy Romo <freddyromochavez@gmail.com>
2025-11-14 19:06:30 +0200

    Update vercel adapter

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from "@astrojs/vercel/server";
 
 export default defineConfig({
+  output: "server",
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
   },
+  adapter: vercel(),
 });
